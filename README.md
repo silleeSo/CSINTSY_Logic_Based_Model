34331b56459cd606b385b9d778843b862d6700ca
Chrysille <chrysille_so@dlsu.edu.ph>
2024-07-16 10:43:29 +0800

    Complete backend and update README (factor map)

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CSINTSY_Logic_Based_Model
 
-Add instructions here
+- To run a simple interactive session of the expert system in SWI-Prolog, consult with "Relationship Advising Expert System.pl"
+- To view the factor map for all logical facts and rules, view this link: https://docs.google.com/spreadsheets/d/17hgRwHnOQ94q5xJQOQkNHQWrVGfnabIiG0LRmNox8YI/edit?usp=sharing
+
--- Relationship Advising Expert System.pl
+++ Relationship Advising Expert System.pl
@@ -7,6 +7,14 @@ mbti(isfj).
 mbti(estj).
 mbti(esfp).
 mbti(istp).
+mbti(intp).
+mbti(entj).
+mbti(infp).
+mbti(enfj).
+mbti(istj).
+mbti(isfp).
+mbti(estp).
+mbti(esfj).
 
 % Facts about attachment styles
 attachment_style(secure).
@@ -27,12 +35,19 @@ interest(traveling).
 interest(sports).
 interest(music).
 interest(cooking).
+interest(art).
+interest(gaming).
+interest(hiking).
 
 % Facts about values
 value(family).
 value(career).
 value(friends).
 value(personal_growth).
+value(adventure).
+value(stability).
+value(independence).
+value(community).
 
 % Facts about communication styles
 communication_style(assertive).
@@ -49,6 +64,14 @@ compatible(isfj, estj).
 compatible(estj, isfj).
 compatible(esfp, istp).
 compatible(istp, esfp).
+compatible(intp, enfj).
+compatible(entj, infp).
+compatible(infp, enfj).
+compatible(enfj, intp).
+compatible(istj, esfj).
+compatible(isfp, estp).
+compatible(estp, isfp).
+compatible(esfj, istj).
 
 % Rules for recommending partner based on MBTI
 recommend_partner(MBTI, Partner) :- compatible(MBTI, Partner).
@@ -62,23 +85,50 @@ recommend_attachment_style(isfj, secure).
 recommend_attachment_style(estj, secure).
 recommend_attachment_style(esfp, secure).
 recommend_attachment_style(istp, avoidant).
+recommend_attachment_style(intp, avoidant).
+recommend_attachment_style(entj, secure).
+recommend_attachment_style(infp, anxious).
+recommend_attachment_style(enfj, secure).
+recommend_attachment_style(istj, secure).
+recommend_attachment_style(isfp, anxious).
+recommend_attachment_style(estp, secure).
+recommend_attachment_style(esfj, secure).
 
 % Rules for recommending communication style based on MBTI
 recommend_communication_style(intj, assertive).
-recommend_communication_style(entp, aggressive).
+recommend_communication_style(entp, assertive).
 recommend_communication_style(infj, passive).
-recommend_communication_style(enfp, passive_aggressive).
+recommend_communication_style(enfp, assertive).
 recommend_communication_style(isfj, passive).
 recommend_communication_style(estj, assertive).
-recommend_communication_style(esfp, passive_aggressive).
-recommend_communication_style(istp, passive).
+recommend_communication_style(esfp, assertive).
+recommend_communication_style(istp, assertive).
+recommend_communication_style(intp, passive).
+recommend_communication_style(entj, assertive).
+recommend_communication_style(infp, passive).
+recommend_communication_style(enfj, assertive).
+recommend_communication_style(istj, assertive).
+recommend_communication_style(isfp, passive).
+recommend_communication_style(estp, assertive).
+recommend_communication_style(esfj, assertive).
 
-% Rules for recommending love language
-recommend_love_language(words_of_affirmation, quality_time).
-recommend_love_language(acts_of_service, receiving_gifts).
-recommend_love_language(receiving_gifts, physical_touch).
-recommend_love_language(quality_time, words_of_affirmation).
-recommend_love_language(physical_touch, acts_of_service).
+% Rules for recommending love language based on MBTI
+recommend_love_language(intj, acts_of_service).
+recommend_love_language(entp, words_of_affirmation).
+recommend_love_language(infj, quality_time).
+recommend_love_language(enfp, words_of_affirmation).
+recommend_love_language(isfj, acts_of_service).
+recommend_love_language(estj, acts_of_service).
+recommend_love_language(esfp, physical_touch).
+recommend_love_language(istp, physical_touch).
+recommend_love_language(intp, quality_time).
+recommend_love_language(entj, acts_of_service).
+recommend_love_language(infp, words_of_affirmation).
+recommend_love_language(enfj, quality_time).
+recommend_love_language(istj, acts_of_service).
+recommend_love_language(isfp, physical_touch).
+recommend_love_language(estp, physical_touch).
+recommend_love_language(esfj, words_of_affirmation).
 
 % Rules for recommending interest
 recommend_interest(Interest, PartnerInterest) :- interest(Interest), interest(PartnerInterest).
@@ -94,7 +144,8 @@ start :-
     format('Based on your MBTI, your attachment style is: ~w', [UserAttachmentStyle]), nl,
     recommend_communication_style(UserMBTI, UserCommunicationStyle),
     format('Based on your MBTI, your communication style is: ~w', [UserCommunicationStyle]), nl,
-    prompt_user(love_language, UserLoveLanguage),
+    recommend_love_language(UserMBTI, UserLoveLanguage),
+    format('Based on your MBTI, your love language is: ~w', [UserLoveLanguage]), nl,
     prompt_user(interest, UserInterest),
     prompt_user(value, UserValue),
     recommend(UserMBTI, UserAttachmentStyle, UserLoveLanguage, UserInterest, UserValue, UserCommunicationStyle).
@@ -114,14 +165,13 @@ format_list([Head|Tail]) :- write(Head), write(', '), format_list(Tail).
 recommend(UserMBTI, UserAttachmentStyle, UserLoveLanguage, UserInterest, UserValue, UserCommunicationStyle) :-
     recommend_partner(UserMBTI, Partner),
     format('Your recommended partner MBTI type is: ~w', [Partner]), nl,
-    recommend_love_language(UserLoveLanguage, LoveLanguageRecommendation),
-    format('Your recommended love language match is: ~w', [LoveLanguageRecommendation]), nl,
     recommend_interest(UserInterest, InterestRecommendation),
     format('Your recommended interest match is: ~w', [InterestRecommendation]), nl,
     recommend_value(UserValue, ValueRecommendation),
     format('Your recommended value match is: ~w', [ValueRecommendation]), nl,
     format('Your recommended attachment style match is: ~w', [UserAttachmentStyle]), nl,
-    format('Your recommended communication style match is: ~w', [UserCommunicationStyle]), nl.
+    format('Your recommended communication style match is: ~w', [UserCommunicationStyle]), nl,
+    format('Your recommended love language match is: ~w', [UserLoveLanguage]), nl.
 
 % Run the program
 :- start.
